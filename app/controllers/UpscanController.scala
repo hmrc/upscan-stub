9ab8f0689c7f278ceb7486574c690eaa2d3c094a
joanna.pintopaul <joanna.pintopaul@digital.hmrc.gov.uk>
2018-05-08 16:01:43 +0100

    PLATY-410: Adding form validation

--- app/controllers/UpscanController.scala
+++ app/controllers/UpscanController.scala
@@ -1,9 +1,13 @@
 package controllers
 
-import akka.util.ByteString
+import java.util.UUID
 import javax.inject.Inject
-import models.{PreparedUpload, Reference, UploadSettings}
+
+import akka.util.ByteString
+import models.{PreparedUpload, Reference, UploadSettings, UploadValues}
 import play.api.Logger
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.http.HttpEntity
 import play.api.libs.json.{Json, _}
 import play.api.mvc.{Action, ResponseHeader, Result}
@@ -11,9 +15,22 @@ import services.{FileStorageService, PrepareUploadService}
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
 import scala.concurrent.Future
+import scala.xml.Node
 
 class UpscanController @Inject()(prepareUploadService: PrepareUploadService, storageService: FileStorageService)
-    extends BaseController {
+  extends BaseController {
+
+  private val uploadForm: Form[UploadValues] = Form(
+    mapping(
+      "x-amz-algorithm" -> nonEmptyText,
+      "x-amz-credential" -> nonEmptyText,
+      "x-amz-date" -> nonEmptyText,
+      "policy" -> nonEmptyText,
+      "x-amz-signature" -> nonEmptyText,
+      "acl" -> nonEmptyText,
+      "key" -> nonEmptyText
+    )(UploadValues.apply)(UploadValues.unapply)
+  )
 
   def prepareUpload(): Action[JsValue] =
     Action.async(parse.json) { implicit request =>
@@ -24,16 +41,24 @@ class UpscanController @Inject()(prepareUploadService: PrepareUploadService, sto
       }
     }
 
-  def upload() = Action(parse.multipartFormData) { request =>
-    val result = for {
-      key  <- request.body.dataParts("key").headOption
-      file <- request.body.file("file")
-    } yield {
-      storageService.store(file.ref, Reference(key))
-      Ok
-    }
+  def upload() = Action(parse.multipartFormData) { implicit request =>
+    val validatedForm: Either[String, UploadValues] = uploadForm.bindFromRequest().fold(
+      formWithErrors => Left(formWithErrors.errors.mkString(", ")),
+      formValues => Right(formValues)
+    )
 
-    result getOrElse BadRequest(Json.obj("error" -> "Form does not contain valid file field"))
+    validatedForm match {
+      case Right(uploaded) =>
+        request.body.file("file") map { uploadFile =>
+          storageService.store(uploadFile.ref, Reference(uploaded.key))
+          NoContent
+        } getOrElse BadRequest(invalidRequestBody(
+          "IncorrectNumberOfFilesInPostRequest",
+          "POST requires exactly one file upload per request."
+        ))
+      case Left(errors) =>
+        BadRequest(invalidRequestBody("InvalidArgument", errors))
+    }
   }
 
   def download(reference: String) = Action {
@@ -42,11 +67,20 @@ class UpscanController @Inject()(prepareUploadService: PrepareUploadService, sto
     } yield {
       Result(
         header = ResponseHeader(200, Map.empty),
-        body   = HttpEntity.Strict(ByteString(source.body), None)
+        body = HttpEntity.Strict(ByteString(source.body), None)
       )
       Ok
     }) getOrElse NotFound
 
   }
 
+
+  private def invalidRequestBody(code: String, message: String): Node = {
+    xml.XML.loadString(s"""<Error>
+      <Code>$code</Code>
+      <Message>$message</Message>
+      <Resource>NoFileReference</Resource>
+      <RequestId>${UUID.randomUUID}</RequestId>
+    </Error>""")
+  }
 }
