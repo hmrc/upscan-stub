b5de01898e477fa661484895f5f56f50423d510b
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2018-05-08 15:53:15 +0100

    PLATY-410 Implemented download functionality, added tests of upload service, disabled metric collection and monitoring

--- app/controllers/UpscanController.scala
+++ app/controllers/UpscanController.scala
@@ -1,20 +1,19 @@
 package controllers
 
-import java.io.File
+import akka.util.ByteString
 import javax.inject.Inject
-
-import models.{PreparedUpload, UploadSettings}
+import models.{PreparedUpload, Reference, UploadSettings}
 import play.api.Logger
+import play.api.http.HttpEntity
 import play.api.libs.json.{Json, _}
-import play.api.mvc.Action
+import play.api.mvc.{Action, ResponseHeader, Result}
 import services.{FileStorageService, PrepareUploadService}
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
 import scala.concurrent.Future
 
-class UpscanController @Inject()(prepareUploadService: PrepareUploadService,
-                                 storageService: FileStorageService
-                                ) extends BaseController {
+class UpscanController @Inject()(prepareUploadService: PrepareUploadService, storageService: FileStorageService)
+    extends BaseController {
 
   def prepareUpload(): Action[JsValue] =
     Action.async(parse.json) { implicit request =>
@@ -26,13 +25,28 @@ class UpscanController @Inject()(prepareUploadService: PrepareUploadService,
     }
 
   def upload() = Action(parse.multipartFormData) { request =>
-    request.body.file("file") flatMap { fileField =>
-      request.body.dataParts.get("key") map { key =>
-        val storeResult: String = storageService.store(fileField.ref, key.head)
-        Ok("File uploaded")
-      }
-    } getOrElse {
-      BadRequest(Json.obj("error" -> "Form does not contain valid file field"))
+    val result = for {
+      key  <- request.body.dataParts("key").headOption
+      file <- request.body.file("file")
+    } yield {
+      storageService.store(file.ref, Reference(key))
+      Ok
     }
+
+    result getOrElse BadRequest(Json.obj("error" -> "Form does not contain valid file field"))
   }
+
+  def download(reference: String) = Action {
+    (for {
+      source <- storageService.get(Reference(reference))
+    } yield {
+      Result(
+        header = ResponseHeader(200, Map.empty),
+        body   = HttpEntity.Strict(ByteString(source.body), None)
+      )
+      Ok
+    }) getOrElse NotFound
+
+  }
+
 }
