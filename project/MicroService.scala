88a9f7ed94bf4b4c5e9079e7ccd247ae919e8ddc
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2018-05-11 08:42:10 +0100

    PLATY-410 Fixed buildsystem so that project can be built with jenkins

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -1,44 +1,55 @@
+import play.routes.compiler.{InjectedRoutesGenerator, StaticRoutesGenerator}
+import play.sbt.PlayImport.PlayKeys
+import play.sbt.routes.RoutesKeys.routesGenerator
 import sbt.Keys._
 import sbt.Tests.{Group, SubProcess}
 import sbt._
-import play.routes.compiler.StaticRoutesGenerator
-import play.sbt.PlayImport.PlayKeys
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
 
 trait MicroService {
 
   import uk.gov.hmrc._
   import DefaultBuildSettings._
-  import uk.gov.hmrc.ShellPrompt
-  import play.sbt.routes.RoutesKeys.routesGenerator
-
   import TestPhases._
+  import scoverage.ScoverageKeys
+  import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
 
   val appName: String
-  val appVersion: String
 
   lazy val appDependencies: Seq[ModuleID] = ???
-  lazy val plugins: Seq[Plugins]          = Seq(play.sbt.PlayScala)
+  lazy val plugins: Seq[Plugins]          = Nil
   lazy val playSettings: Seq[Setting[_]]  = Seq.empty
 
+  routesGenerator := InjectedRoutesGenerator
+
+  lazy val scoverageSettings = {
+    Seq(
+      // Semicolon-separated list of regexs matching classes to exclude
+      ScoverageKeys.coverageExcludedPackages := "<empty>;Reverse.*;.*AuthService.*;models/.data/..*;view.*",
+      ScoverageKeys.coverageExcludedFiles :=
+        ".*/frontendGlobal.*;.*/frontendAppConfig.*;.*/frontendWiring.*;.*/views/.*_template.*;.*/govuk_wrapper.*;.*/routes_routing.*;.*/BuildInfo.*",
+      // Minimum is deliberately low to avoid failures initially - please increase as we add more coverage
+      ScoverageKeys.coverageMinimum := 25,
+      ScoverageKeys.coverageFailOnMinimum := false,
+      ScoverageKeys.coverageHighlighting := true,
+      parallelExecution in Test := false
+    )
+  }
+
   lazy val microservice = Project(appName, file("."))
-    .enablePlugins(plugins: _*)
+    .enablePlugins(Seq(play.sbt.PlayScala) ++ plugins: _*)
     .settings(PlayKeys.playDefaultPort := 9570)
     .settings(playSettings: _*)
-    .settings(version := appVersion)
-    .settings(scalaSettings: _*)
+    .settings(scalaSettings ++ scoverageSettings: _*)
     .settings(publishingSettings: _*)
     .settings(defaultSettings(): _*)
     .settings(
       targetJvm := "jvm-1.8",
-      shellPrompt := ShellPrompt(appVersion),
       libraryDependencies ++= appDependencies,
       parallelExecution in Test := false,
       fork in Test := false,
       retrieveManaged := true,
       routesGenerator := StaticRoutesGenerator
     )
-    .settings(Repositories.playPublishingSettings: _*)
     .configs(IntegrationTest)
     .settings(inConfig(IntegrationTest)(Defaults.itSettings): _*)
     .settings(
@@ -48,12 +59,10 @@ trait MicroService {
       testGrouping in IntegrationTest := oneForkedJvmPerTest((definedTests in IntegrationTest).value),
       parallelExecution in IntegrationTest := false
     )
-    .settings(SbtBuildInfo(): _*)
-    .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
-    .settings(resolvers ++= Seq(
-      Resolver.bintrayRepo("hmrc", "releases"),
-      Resolver.jcenterRepo
-    ))
+    .settings(
+      resolvers += Resolver.bintrayRepo("hmrc", "releases"),
+      resolvers += Resolver.jcenterRepo
+    )
 }
 
 private object TestPhases {
@@ -62,19 +71,4 @@ private object TestPhases {
     tests map { test =>
       new Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name))))
     }
-}
-
-private object Repositories {
-
-  import uk.gov.hmrc._
-  import PublishingSettings._
-  import NexusPublishing._
-
-  lazy val playPublishingSettings: Seq[sbt.Setting[_]] = sbtrelease.ReleasePlugin.releaseSettings ++ Seq(
-    credentials += SbtCredentials,
-    publishArtifact in (Compile, packageDoc) := false,
-    publishArtifact in (Compile, packageSrc) := false
-  ) ++
-    publishAllArtefacts ++
-    nexusPublishingSettings
 }
