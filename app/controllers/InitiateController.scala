1c2e154832de7cd90428bb8005c5d2b0e6d54d95
Tim Golding <tim.golding@digital.hmrc.gov.uk>
2018-05-14 13:28:32 +0100

    PLATY-410: Added default values for file size min/max conditions. Added integration tests for file size.

--- app/controllers/InitiateController.scala
+++ app/controllers/InitiateController.scala
@@ -1,10 +1,12 @@
 package controllers
 
 import javax.inject.Inject
-
 import model.UploadSettings
 import play.api.Logger
-import play.api.libs.json.{JsValue, Json}
+import play.api.data.validation.ValidationError
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
+import play.api.libs.json.{JsPath, JsValue, Json, _}
 import play.api.mvc.Action
 import services.PrepareUploadService
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
@@ -13,6 +15,17 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class InitiateController @Inject()(prepareUploadService: PrepareUploadService)(implicit ec: ExecutionContext)
     extends BaseController {
+
+  implicit val uploadSettingsReads: Reads[UploadSettings] = (
+    (JsPath \ "callbackUrl").read[String] and
+      (JsPath \ "minimumFileSize").readNullable[Int](min(PrepareUploadService.minFileSize)) and
+      (JsPath \ "maximumFileSize").readNullable[Int](min(PrepareUploadService.minFileSize) keepAnd max(PrepareUploadService.maxFileSize)) and
+      (JsPath \ "expectedContentType").readNullable[String]
+    )(UploadSettings.apply _)
+    .filter(ValidationError("Maximum file size must be equal or greater than minimum file size"))(
+      settings =>
+        settings.minimumFileSize.getOrElse(0) <= settings.maximumFileSize.getOrElse(PrepareUploadService.maxFileSize)
+    )
 
   def prepareUpload(): Action[JsValue] =
     Action.async(parse.json) { implicit request =>
--- it/controllers/InitiateControllerISpec.scala
+++ it/controllers/InitiateControllerISpec.scala
@@ -5,7 +5,8 @@ import akka.stream.ActorMaterializer
 import model.PreparedUpload
 import org.scalatest.GivenWhenThen
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.libs.json.Json
+import play.api.http.Status
+import play.api.libs.json.{JsValue, Json}
 import play.api.test.Helpers.{contentAsJson, route}
 import play.api.test.{FakeHeaders, FakeRequest, Helpers}
 import uk.gov.hmrc.play.test.UnitSpec
@@ -18,11 +19,12 @@ class InitiateControllerISpec extends UnitSpec with GuiceOneAppPerSuite with Giv
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val timeout: akka.util.Timeout      = 10.seconds
 
-  "InitiateController" should {
+  "InitiateController /initiate" should {
 
     "respond with expected success JSON when passed a valid request" in {
       Given("a valid request JSON body")
-      val postBodyJson = Json.parse("""
+      val postBodyJson = Json.parse(
+        """
           |{
           |	"callbackUrl": "http://localhost:9570/callback",
           |	"minimumFileSize" : 0,
@@ -46,19 +48,20 @@ class InitiateControllerISpec extends UnitSpec with GuiceOneAppPerSuite with Giv
 
       (responseJson \ "uploadRequest" \ "href").as[String] shouldBe "http:///upscan/upload"
       (responseJson \ "uploadRequest" \ "fields" \ "x-amz-meta-callback-url")
-        .as[String]                           shouldBe "http://localhost:9570/callback"
+        .as[String] shouldBe "http://localhost:9570/callback"
       (responseJson \ "reference").as[String] shouldBe (responseJson \ "uploadRequest" \ "fields" \ "key").as[String]
     }
 
     "respond with expected error JSON when passed a invalid request" in {
       Given("an invalid request JSON body")
-      val postBodyJson = Json.parse("""
-                                      |{
-                                      |	"someKey": "someValue",
-                                      |	"someOtherKey" : 0,
-                                      |	"expectedMimeType": "application/xml"
-                                      |}
-                                    """.stripMargin)
+      val postBodyJson = Json.parse(
+        """
+          |{
+          |	"someKey": "someValue",
+          |	"someOtherKey" : 0,
+          |	"expectedMimeType": "application/xml"
+          |}
+        """.stripMargin)
 
       val initiateRequest =
         FakeRequest(Helpers.POST, "/upscan/initiate", FakeHeaders(), postBodyJson)
@@ -73,5 +76,103 @@ class InitiateControllerISpec extends UnitSpec with GuiceOneAppPerSuite with Giv
       val responseBody: String = bodyOf(initiateResponse)
       responseBody shouldBe "Invalid UploadSettings payload: List((/callbackUrl,List(ValidationError(List(error.path.missing),WrappedArray()))))"
     }
+
+    "respond with supplied file size constraints in the policy" in {
+      Given("a valid request with file size constraints")
+      val postBodyJson = Json.obj(
+        "callbackUrl" -> "myCallbackUrl",
+        "minimumFileSize" -> 123,
+        "maximumFileSize" -> 456,
+        "expectedContentType" -> "pdf"
+      )
+
+      val initiateRequest = FakeRequest(Helpers.POST, "/upscan/initiate", FakeHeaders(), postBodyJson)
+
+      When("the request is POSTed to /upscan/initiate")
+      val initiateResponse = route(app, initiateRequest).get
+
+      Then("the response status is 200")
+      status(initiateResponse) shouldBe Status.OK
+
+      And("the response policy includes the supplied file size constraints")
+      withMinMaxFileSizesInPolicyConditions(contentAsJson(initiateResponse)) { (min, max) =>
+        min shouldBe Some(123)
+        max shouldBe Some(456)
+      }
+    }
+
+    "respond with default file size constraints in the policy when supplied values are missing" in {
+      Given("a valid request with no file size constraints")
+      val postBodyJson = Json.obj(
+        "callbackUrl" -> "myCallbackUrl",
+        "expectedContentType" -> "pdf"
+      )
+
+      val initiateRequest = FakeRequest(Helpers.POST, "/upscan/initiate", FakeHeaders(), postBodyJson)
+
+      When("the request is POSTed to /upscan/initiate")
+      val initiateResponse = route(app, initiateRequest).get
+
+      Then("the response status is 200")
+      status(initiateResponse) shouldBe Status.OK
+
+      And("the response policy includes the default file size constraints")
+      withMinMaxFileSizesInPolicyConditions(contentAsJson(initiateResponse)) { (min, max) =>
+        min shouldBe Some(0)
+        max shouldBe Some(104857600)
+      }
+    }
+
+    "respond with 400 when supplied values are outside of expected limits" in {
+      Given("an invalid request with invalid file size limits")
+      val postBodyJson = Json.obj(
+        "callbackUrl" -> "myCallbackUrl",
+        "minimumFileSize" -> -10,
+        "maximumFileSize" -> 104857600*10,
+        "expectedContentType" -> "pdf"
+      )
+
+      val initiateRequest = FakeRequest(Helpers.POST, "/upscan/initiate", FakeHeaders(), postBodyJson)
+
+      When("the request is POSTed to /upscan/initiate")
+      val initiateResponse = route(app, initiateRequest).get
+
+      Then("the response status is 400")
+      status(initiateResponse) shouldBe Status.BAD_REQUEST
+    }
+
+    "respond with 400 when supplied min value is greater than the max value" in {
+      Given("an invalid request with invalid file size limits")
+      val postBodyJson = Json.obj(
+        "callbackUrl" -> "myCallbackUrl",
+        "minimumFileSize" -> 100,
+        "maximumFileSize" -> 90,
+        "expectedContentType" -> "pdf"
+      )
+
+      val initiateRequest = FakeRequest(Helpers.POST, "/upscan/initiate", FakeHeaders(), postBodyJson)
+
+      When("the request is POSTed to /upscan/initiate")
+      val initiateResponse = route(app, initiateRequest).get
+
+      Then("the response status is 400")
+      status(initiateResponse) shouldBe Status.BAD_REQUEST
+    }
+  }
+
+  private def withMinMaxFileSizesInPolicyConditions[T](responseBodyJson: JsValue)(block: (Option[Long], Option[Long]) => T): T = {
+    import utils.Implicits.Base64StringOps
+
+    val maybeBase64policy = (responseBodyJson \ "uploadRequest" \ "fields" \ "policy").asOpt[String]
+
+    val policy: String = maybeBase64policy.get.base64decode
+
+    val policyJson: JsValue = Json.parse(policy)
+
+    val conditions = (policyJson \ "conditions")(0)
+
+    conditions(0).asOpt[String] shouldBe Some("content-length-range")
+
+    block(conditions(1).asOpt[Long], conditions(2).asOpt[Long])
   }
 }
