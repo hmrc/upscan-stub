0db099007ef50331d389279c9aea9b4deb60cab5
joanna.pintopaul <joanna.pintopaul@digital.hmrc.gov.uk>
2018-05-14 16:04:33 +0100

    PLATY-410: DownloadController integration tests

---
+++ it/controllers/DownloadControllerISpec.scala
@@ -0,0 +1,57 @@
+package controllers
+
+import java.nio.file.{Files, Paths}
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import model.Reference
+import org.scalatest.GivenWhenThen
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.libs.Files.TemporaryFile
+import play.api.test.Helpers.{route, _}
+import play.api.test.{FakeRequest, Helpers}
+import services.FileStorageService
+import uk.gov.hmrc.play.test.UnitSpec
+import scala.concurrent.duration._
+
+class DownloadControllerISpec extends UnitSpec with GuiceOneAppPerSuite with GivenWhenThen {
+
+  implicit val actorSystem: ActorSystem        = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val timeout: akka.util.Timeout      = 10.seconds
+
+  "DownloadController" should {
+
+    "download a file" in {
+      Given("a reference to a previously stored file")
+
+      val path = Paths.get("it/resources/my-it-file.txt")
+      val file = Files.createFile(path).toFile
+      Files.write(path, "Integration test file contents".getBytes)
+
+      val storageService = app.injector.instanceOf[FileStorageService]
+      storageService.store(new TemporaryFile(file), Reference("my-it-file"))
+
+      val downloadRequest = FakeRequest(Helpers.GET, "/upscan/download/my-it-file")
+
+      When("a GET request is made to /download/:reference endpoint")
+      val downloadResponse = route(app, downloadRequest).get
+
+      Then("the uploaded file is successfully retrieved")
+      status(downloadResponse) shouldBe 200
+      val downloadContents: String = bodyOf(downloadResponse)
+      downloadContents shouldBe "Integration test file contents"
+    }
+
+    "return Not Found for invalid file reference" in {
+      Given("an invalid file reference")
+      val downloadRequest = FakeRequest(Helpers.GET, "/upscan/download/my-invalid-file")
+
+      When("a GET request is made to /download/:reference endpoint")
+      val downloadResponse = route(app, downloadRequest).get
+
+      Then("a Not Found response should be returned")
+      status(downloadResponse) shouldBe 404
+    }
+  }
+}
