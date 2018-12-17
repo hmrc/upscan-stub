18a6d6312d8010e916a4b1b5703215fd5b48f23b
Tim Golding <tim.golding@digital.hmrc.gov.uk>
2018-12-17 16:36:24 +0000

    PLATOPS-1874: Added logging for debugging issues when running via Service Manager in Jenkins.

--- app/controllers/DownloadController.scala
+++ app/controllers/DownloadController.scala
@@ -3,6 +3,7 @@ package controllers
 import akka.util.ByteString
 import javax.inject.Inject
 import model.FileId
+import play.api.Logger
 import play.api.http.HttpEntity
 import play.api.mvc.{Action, ResponseHeader, Result}
 import services.FileStorageService
@@ -11,7 +12,8 @@ import uk.gov.hmrc.play.bootstrap.controller.BaseController
 class DownloadController @Inject()(storageService: FileStorageService) extends BaseController {
 
   def download(fileId: String) = Action {
-    (for {
+
+    val result: Result = (for {
       source <- storageService.get(FileId(fileId))
     } yield {
       Result(
@@ -20,6 +22,8 @@ class DownloadController @Inject()(storageService: FileStorageService) extends B
       )
     }) getOrElse NotFound
 
-  }
+    Logger.debug(s"Download request for file reference: [${fileId}], returning status: [${result.header.status}].")
 
+    result
+  }
 }
