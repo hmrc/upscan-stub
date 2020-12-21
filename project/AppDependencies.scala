e129f480885144a31721d8971ef0f5d2e7f0ed5c
Nigel Perkins <nigelhp@users.noreply.github.com>
2020-12-21 13:09:34 +0000

    BDOG-632: dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,19 +6,19 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"               %% "bootstrap-backend-play-27" % "2.24.0",
-    "org.apache.httpcomponents" %  "httpclient"                % "4.5.11"
+    "uk.gov.hmrc"               %% "bootstrap-backend-play-27" % "3.2.0",
+    "org.apache.httpcomponents" %  "httpclient"                % "4.5.13"
   )
 
   def test(scope: String = "test,it") = Seq(
-    "uk.gov.hmrc"            %% "service-integration-test"    % "0.12.0-play-27"    % scope,
+    "uk.gov.hmrc"            %% "service-integration-test"    % "0.13.0-play-27"    % scope,
     "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % scope,
     "com.typesafe.play"      %% "play-ws"                     % PlayVersion.current % scope,
-    "org.mockito"            %% "mockito-scala"               % "1.13.1"            % scope,
-    "commons-io"             % "commons-io"                   % "2.6"               % scope,
-    "org.scalacheck"         %% "scalacheck"                  % "1.13.4"            % scope,
-    "com.github.tomakehurst" %  "wiremock-jre8"               % "2.21.0"            % scope,
-    "org.scalatest"          %% "scalatest"                   % "3.1.1"             % scope,
+    "org.mockito"            %% "mockito-scala"               % "1.16.3"            % scope,
+    "commons-io"             % "commons-io"                   % "2.8.0"             % scope,
+    "org.scalacheck"         %% "scalacheck"                  % "1.15.2"            % scope,
+    "com.github.tomakehurst" %  "wiremock-jre8"               % "2.27.2"            % scope,
+    "org.scalatest"          %% "scalatest"                   % "3.1.4"             % scope,
     "org.scalamock"          %% "scalamock-scalatest-support" % "3.5.0"             % scope,
     "org.scalatestplus.play" %% "scalatestplus-play"          % "4.0.3"             % scope,
     "com.vladsch.flexmark"   %  "flexmark-all"                % "0.35.10"           % scope
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -8,10 +8,10 @@ addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.13")
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.5")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "2.9.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "2.12.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.1.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.2.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.0.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
