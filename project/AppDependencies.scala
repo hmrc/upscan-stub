4d325794ade2addf8e768e36747d5cdc957e2f58
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2025-03-06 16:58:41 +0000

    Bump dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,9 +1,7 @@
 import sbt._
 
 object AppDependencies {
-  import play.sbt.PlayImport.ws
-
-  val bootstrapVersion = "9.5.0"
+  val bootstrapVersion = "9.11.0"
 
   val compile = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVersion,
